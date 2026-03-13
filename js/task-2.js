4a574535259ed0c46dfe36f4036a1fa195976fc2
Vladyslav Huminiuk <vldgum@gmail.com>
2026-03-13 17:31:38 +0200

    test(1,2,3)

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+function makeArray(firstArray, secondArray, maxLength){
+    if (maxLength <= firstArray.length + secondArray.length) {
+        return firstArray.concat(secondArray).slice(0, maxLength);
+    }
+    return firstArray.concat(secondArray);
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
